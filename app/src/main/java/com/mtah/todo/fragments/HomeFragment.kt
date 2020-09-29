186ad7ea0441b93983f881a3f428223aff8e06f4
mustaphaattah <mustaphaattah@gmail.com>
2020-09-29 15:08:06 -0400

    design for action bar for home fragment

--- app/src/main/java/com/mtah/todo/fragments/HomeFragment.kt
+++ app/src/main/java/com/mtah/todo/fragments/HomeFragment.kt
@@ -1,10 +1,8 @@
 package com.mtah.todo.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.mtah.todo.R
 import kotlinx.android.synthetic.main.fragment_home.view.*
@@ -30,4 +28,7 @@ class HomeFragment : Fragment() {
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_fragment_menu, menu)
+    }
 }
