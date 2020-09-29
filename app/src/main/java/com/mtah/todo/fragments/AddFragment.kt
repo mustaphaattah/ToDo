f03f3f752b325c4b599e2ba89fab36e7bc8bb8ce
mustaphaattah <mustaphaattah@gmail.com>
2020-09-29 12:56:42 -0400

    design for Add fragment

---
+++ app/src/main/java/com/mtah/todo/fragments/AddFragment.kt
@@ -0,0 +1,24 @@
+package com.mtah.todo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mtah.todo.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
+
+
+        return view
+    }
+}
