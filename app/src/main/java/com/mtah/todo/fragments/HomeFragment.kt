7363786c3594c42b4997cef2471996003ac4c5df
mustaphaattah <mustaphaattah@gmail.com>
2020-09-29 12:54:19 -0400

    design for home fragment

---
+++ app/src/main/java/com/mtah/todo/fragments/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.mtah.todo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mtah.todo.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addFragment)
+        }
+
+
+        return view
+    }
+
+}
